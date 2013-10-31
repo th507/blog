94facb41835873966c3c27eadc3e184a4ae9104b
Jingwei "John" Liu <liujingwei@gmail.com>
2013-11-01 01:13:32 +0800

    fix title blur when resized

--- js/style.js
+++ js/style.js
@@ -1,32 +1,54 @@
 (function() {
+    /*jshint curly:false*/
     var hasBlur = false;
     var blurTitle;
+    var mouseover = "mouseover";
+    var resize = "resize";
+    var DOMContentLoaded = "DOMContentLoaded";
     // add blurry effect for posts with longer titles
     blurTitle = function () {
         if (hasBlur) return;
         try {
             var fourth = document.getElementsByClassName("fourth");
+            fourth = [].slice.call(fourth);
             var addBlurry;
 
-            addBlurry = function (evt) {
-                try {
-                    var el = this;
-                    if (el.offsetWidth + 10 >= el.scrollWidth) return;
-                    el.nextElementSibling.firstElementChild.classList.add("blurry");
-                    el.removeEventListener("mouseover", addBlurry);
-                } catch (e) {}
+            addBlurry = function () {
+                var el = this;
+                // check only once
+                el.removeEventListener(mouseover, addBlurry);
+
+                // boolean toggler for blurry effect
+                var index = 0 + (el.offsetWidth + 10 > el.scrollWidth);
+
+                // add or remove blurry effect
+                var method = ["add", "remove"];
+
+                el.nextElementSibling.firstElementChild.classList[method[index]]("blurry");
             };
 
-            [].forEach.call(fourth, function(el) {
-                el.addEventListener("mouseover", addBlurry);
-            });
+            function checkTitleHover() {
+                fourth.forEach(function(el) {
+                    el.addEventListener(mouseover, addBlurry);
+                });
+            }
+            // initial check
+            checkTitleHover();
+
+            // toggler for initial check
+            // never run initial check twice
             hasBlur = true;
 
-            window.removeEventListener("DOMContentLoaded", blurTitle);
+            // extra check for every resize
+            window.addEventListener(resize, checkTitleHover);
+
+            window.removeEventListener(DOMContentLoaded, blurTitle);
         } catch (e) {}
     };
 
-    window.addEventListener("DOMContentLoaded", blurTitle);
+    try {
+        window.addEventListener(DOMContentLoaded, blurTitle);
+    } catch (e) {}
 
     blurTitle();
 }());
